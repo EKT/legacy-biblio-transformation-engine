6f9bde27a3908946fba4af35500236ed9f1456b0
nhoussos@ekt.gr <nhoussos@ekt.gr@4a23643d-4f34-e410-4d99-68e0cf7e7863>
2012-08-29 15:53:37 +0000

    Added header for New BSD license.

--- trunk/src/main/java/gr/ekt/transformationengine/core/Filter.java
+++ trunk/src/main/java/gr/ekt/transformationengine/core/Filter.java
@@ -1,13 +1,31 @@
-/** 
- * Filter.java
- *
- * Version: 1
- *
- * Date: $Date: 2007-9-21 (Fri, 21 Sep 2007) $
- *
- * This is the core class having the main method 
- *
- */
+// Copyright (c) 2007-2012 National Documentation Centre (EKT, www.ekt.gr)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//   1. Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+// 
+//   2. Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+// 
+//   3. The name of the author may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+///////////////////////////////////////////////////////////////////////////////
 
 package gr.ekt.transformationengine.core;
 
@@ -19,6 +37,17 @@ import gr.ekt.transformationengine.exceptions.UnsupportedCriterion;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+/** 
+ * Filter.java
+ *
+ * Version: 1
+ *
+ * Date: $Date: 2007-9-21 (Fri, 21 Sep 2007) $
+ *
+ * This is the core class having the main method 
+ *
+ */
 
 /** 
  * Abstracts the filtering process providing the basic functionality that 
